abfc19bbe8fe7f78f6a84e46a8359feadc85409d
ggambopce <ggambopce@gmail.com>
2025-02-07 11:11:54 +0900

    collaction 연습

---
+++ src/collection/array/ArrayMain1.java
@@ -0,0 +1,13 @@
+package collection.array;
+
+public class ArrayMain1 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5]; // 배열 생성
+        //index 입력: 0(1)
+        System.out.println("==index 입력: 0(1)==");
+
+
+
+    }
+}
