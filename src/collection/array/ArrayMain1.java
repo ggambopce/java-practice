2b00728db5739a7a9824f87ba20170d3a362ec6c
ggambopce <ggambopce@gmail.com>
2025-02-07 11:17:51 +0900

    array 연습

--- src/collection/array/ArrayMain1.java
+++ src/collection/array/ArrayMain1.java
@@ -1,11 +1,18 @@
 package collection.array;
 
+import java.util.Arrays;
+
 public class ArrayMain1 {
 
     public static void main(String[] args) {
         int[] arr = new int[5]; // 배열 생성
         //index 입력: 0(1)
         System.out.println("==index 입력: 0(1)==");
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        System.out.println(Arrays.toString(arr));
+
 
 
 
