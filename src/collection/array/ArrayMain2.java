62036e79433da630583e9b2d8b2e4e7a5b036ac1
ggambopce <ggambopce@gmail.com>
2025-02-08 09:28:57 +0900

    배열의 첫번째 위치에 추가

---
+++ src/collection/array/ArrayMain2.java
@@ -0,0 +1,28 @@
+package collection.array;
+
+import java.util.Arrays;
+
+public class ArrayMain2 {
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        arr[0] = 1;
+        arr[1] = 2;
+        System.out.println(Arrays.toString(arr));
+
+        //배열의 첫번째 위치에 추가
+        //기본 배열의 데이터를 한 칸씩 뒤로 밀고 배열의 첫번째 위치에 추가
+        System.out.println("배열의 첫번째 위치에 3 추가 O(n)");
+        int newValue = 3;
+        addFirst(arr, newValue);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+    private static void addFirst(int[] arr, int newValue) {
+        for (int i = arr.length-1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        arr[0] = newValue;
+    }
+}
